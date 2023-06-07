8b8aa7d4d62af99c81d06123bacbe0d414aae306
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-07 12:44:10 +0000

    Basic setup done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'server-manager';
+  servers: string[] = [];
+
+  onAddServer() {
+    this.servers.push('Another Server');
+  }
+
+  onRemoveServer(id: number) {
+    const position = id + 1;
+    this.servers.splice(position, 1);
+  }
 }
