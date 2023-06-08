76d4dd5219bc114009073989b8e4d212863565bc
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-08 08:32:59 +0000

    Implemented databinding and event binding

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./cockpit.component.scss']
 })
 export class CockpitComponent {
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  newServerName = '';
+  newServerContent = '';
 
+  onAddServer() {
+    this.serverCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
+    });
+  }
+
+  onAddBlueprint() {
+    this.blueprintCreated.emit({
+      serverName: this.newServerName, 
+      serverContent: this.newServerContent
+    });
+  }
 }
