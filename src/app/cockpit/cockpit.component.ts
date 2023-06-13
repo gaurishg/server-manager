db60ad3df363c235ac737827c445df5fe0499c39
Gaurish Gangwar <gaurishgangwar@gmail.com>
2023-06-13 15:12:49 +0000

    Now the app works using local references

--- src/app/cockpit/cockpit.component.ts
+++ src/app/cockpit/cockpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-cockpit',
@@ -9,19 +9,22 @@ export class CockpitComponent {
   @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   @Output('bpCreated') blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
   newServerName = '';
-  newServerContent = '';
+  // newServe''rContent = '';
+  @ViewChild('serverContentInput') serverContentInput!: ElementRef;
 
   onAddServer(serverNameInput: HTMLInputElement) {
     this.serverCreated.emit({
       serverName: serverNameInput.value, 
-      serverContent: this.newServerContent
+      // serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 
   onAddBlueprint(blueprintNameInput: HTMLInputElement) {
     this.blueprintCreated.emit({
       serverName: blueprintNameInput.value, 
-      serverContent: this.newServerContent
+      // serverContent: this.newServerContent
+      serverContent: this.serverContentInput.nativeElement.value
     });
   }
 }
